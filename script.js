5c3aff38e0840c356cd10cda82cb30ad5873cd5c
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-12-03 19:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function validEmail(str) {
+function validEmail(email) {
   //your JS code here.
+	// Regular expression for validating an Email address
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  // Check if the email matches the regular expression
+  return emailRegex.test(email);
 }
 
 // Do not change the code below.
